0f772416b122563dac9864c9aa758bb5df90c12e
mithsv20 <mithsv20@gmail.com>
2024-11-26 18:35:36 +0530

    sort 012 update

--- Array/Sort012.py
+++ Array/Sort012.py
@@ -2,18 +2,18 @@ from collections import deque
 arr = [0, 1, 2, 0, 1, 2]
 
 # Brute force approach
-# arr1 = []
-# arr2 = []
-# arr3 = []
-# for a in arr:
-#     if a == 0:
-#         arr1.append(a)
-#     elif a == 1:
-#         arr2.append(a)
-#     else:
-#         arr3.append(a)
-# arr = arr1+arr2+arr3
-# print(arr)
+arr1 = []
+arr2 = []
+arr3 = []
+for a in arr:
+    if a == 0:
+        arr1.append(a)
+    elif a == 1:
+        arr2.append(a)
+    else:
+        arr3.append(a)
+new_arr1 = arr1+arr2+arr3
+print(arr)
 
 # Insertion Sort Approach
 new_arr = deque()
@@ -27,9 +27,3 @@ for a in arr:
     else:
         new_arr.insert(i, a)
 print(list(new_arr))
-
-
-# BASE(in hand)
-# 10 % Variable
-# Employee PF 12% of basic
-#
